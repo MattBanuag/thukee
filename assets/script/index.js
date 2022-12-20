0c1702e3cabbdacd942ea247084c9acb8c8b51d4
MattBanuag <matthewbanuag.dev@gmail.com>
2022-12-20 09:40:19 -0600

    add loading feat

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,3 +1,18 @@
 'use strict';
-import { select, onEvent, create } from './utils.js';
+import { select, selectAll, onEvent, create } from './utils.js';
 
+// HTML DOC BRIDGE
+const seeMoreBtn = select('.see-more-btn');
+const loader = select('.loader');
+const hiddenBlogs = Array.from(selectAll('.hidden'));
+
+// EVENT LISTENERS
+onEvent('click', seeMoreBtn, () => {
+    loader.classList.remove('hidden');
+    setInterval(() => {
+        hiddenBlogs.forEach((blog) => {
+            blog.classList.remove('hidden');
+            loader.classList.add('hidden');
+        });
+    }, 2000);
+});
